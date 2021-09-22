a69aa5560a4c9edae9c7858c91f1852ce1e9a96d
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-09-22 11:33:19 +0200

    Move plots to their own module

---
+++ devidisc_ui/basic_ui/plots.py
@@ -0,0 +1,54 @@
+import math
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import io, base64
+
+def encode_plot(fig):
+    flike = io.BytesIO()
+    fig.savefig(flike)
+    b64 = base64.b64encode(flike.getvalue()).decode()
+    return b64
+
+def make_discoveries_per_batch_plot(batches):
+    objs = batches
+
+    batch_idx = list(range(0, len(objs)))
+    counts = [x.discovery_set.count() for x in objs]
+
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.plot(batch_idx, counts, '--bo')
+
+    ax.set_title('Discoveries per Batch')
+    ax.set_ylabel("# Discoveries")
+    ax.set_xlabel("Batch Index")
+    ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
+
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    return encode_plot(fig)
+
+def make_interestingness_histogramm_plot(measurements):
+    interestingnesses = [ m.interestingness for m in measurements ]
+
+    finite_entries = [i for i in interestingnesses if math.isfinite(i)]
+    if len(finite_entries) == 0:
+        return None
+    max_finite = max(finite_entries)
+    inf_val = abs(max_finite) * 1.5
+
+    entries = [i if math.isfinite(i) else inf_val for i in interestingnesses]
+
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.hist(entries)
+
+    ax.set_title('Interestingness of Samples')
+    ax.set_ylabel("# Occurrences")
+    ax.set_xlabel("Interestingness")
+    ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
+
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    return encode_plot(fig)
--- devidisc_ui/basic_ui/views.py
+++ devidisc_ui/basic_ui/views.py
@@ -5,17 +5,10 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.utils.safestring import mark_safe
 
-import math
 from pathlib import Path
 
 import django_tables2 as tables
 from markdown import markdown
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
-import io, base64
 
 from devidisc.abstractblock import AbstractBlock
 from devidisc.abstractioncontext import AbstractionContext
@@ -24,6 +17,7 @@ from devidisc.configurable import config_diff
 from .models import Campaign, Discovery
 from .custom_pretty_printing import prettify_absblock, prettify_seconds, prettify_config_diff, prettify_abstraction_config
 
+from .plots import make_discoveries_per_batch_plot, make_interestingness_histogramm_plot
 
 def get_docs(site_name):
     # cached = CACHE.get(site_name, None)
@@ -48,54 +42,6 @@ def get_docs(site_name):
     # CACHE.set(site_name, res)
 
     return res
-
-
-def encode_plot(fig):
-    flike = io.BytesIO()
-    fig.savefig(flike)
-    b64 = base64.b64encode(flike.getvalue()).decode()
-    return b64
-
-def make_discoveries_per_batch_plot(batches):
-    objs = batches
-
-    batch_idx = list(range(0, len(objs)))
-    counts = [x.discovery_set.count() for x in objs]
-
-    fig, ax = plt.subplots(figsize=(10,4))
-    ax.plot(batch_idx, counts, '--bo')
-
-    ax.set_title('Discoveries per Batch')
-    ax.set_ylabel("# Discoveries")
-    ax.set_xlabel("Batch Index")
-    ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
-
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-
-    return encode_plot(fig)
-
-def make_interestingness_histogramm_plot(measurements):
-    interestingnesses = [ m.interestingness for m in measurements ]
-
-    finite_entries = [i for i in interestingnesses if math.isfinite(i)]
-    if len(finite_entries) == 0:
-        return None
-    max_finite = max(finite_entries)
-    inf_val = abs(max_finite) * 1.5
-
-    entries = [i if math.isfinite(i) else inf_val for i in interestingnesses]
-
-    fig, ax = plt.subplots(figsize=(10,4))
-    ax.hist(entries)
-
-    ax.set_title('Interestingness of Samples')
-    ax.set_ylabel("# Occurrences")
-    ax.set_xlabel("Interestingness")
-    ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
-
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-
-    return encode_plot(fig)
 
 
 def load_abstract_block(json_dict, actx):
