5f5f450c8d5b0d5bdeff2479288d6267718832e0
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2022-03-09 15:45:40 +0100

    Bump anica version

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+set -ex
+
 MY_ENV_PATH=./env/anica_ui
 
 echo "Setting up the virtual environment..."
@@ -8,6 +14,9 @@ python3 -m venv $MY_ENV_PATH
 source $MY_ENV_PATH/bin/activate
 
 echo "Installing requirements..."
+
+pip install wheel
+
 pip install -r requirements.txt
 
 pip install ipython
